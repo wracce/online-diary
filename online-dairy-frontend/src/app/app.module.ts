4a65485b82168d3425f87e1659feeb808d4cedf2
wracce <wracce@gmail.com>
2023-04-06 20:22:43 +0400

    Add ng-bootstrap and ngx-editor

--- online-dairy-frontend/src/app/app.module.ts
+++ online-dairy-frontend/src/app/app.module.ts
@@ -3,14 +3,24 @@ import { BrowserModule } from '@angular/platform-browser';
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
+import { HeaderComponent } from './shared/components/header/header.component';
+import { DesktopPageComponent } from './pages/desktop-page/desktop-page.component';
+import { NoteCardComponent } from './pages/desktop-page/desktop-page-src/components/note-card/note-card.component';
+import { NgxEditorModule } from 'ngx-editor';
+import { NgbModule } from '@ng-bootstrap/ng-bootstrap';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    HeaderComponent,
+    DesktopPageComponent,
+    NoteCardComponent,
   ],
   imports: [
     BrowserModule,
-    AppRoutingModule
+    AppRoutingModule,
+    NgxEditorModule,
+    NgbModule
   ],
   providers: [],
   bootstrap: [AppComponent]
